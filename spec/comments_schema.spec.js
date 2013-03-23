95061ed08e79ee123ee4d5158490946b254a59c1
jwang <wangjun0125@gmail.com>
2013-03-23 06:40:38 -0700

    finish comment_schema unit test

--- spec/comments_schema.spec.js
+++ spec/comments_schema.spec.js
@@ -1,6 +1,50 @@
-var Shop_Schema = require("../lib/easydian/model/comment_schema")
+var Comment_Schema = require("../lib/easydian/model/comment_schema")
 require('../lib/easydian/config')
 
-describe("Comments Schema", function(){
-  var cs, doc;
+describe("Comment Schema", function(){
+  //var ExistID = '514ace690a16f3140c000001';
+  var ExistID = '514b0e2c8b4309c20a000001';
+  var cs = new Comment_Schema();
+  var doc = {
+  	shopid : ExistID,
+  	comment: "This shop is great!"
+  };
+  var category = "Dining";
+  it("constructor: can create an instance", function(){
+    expect(typeof cs.comm_schema).toEqual("object");
+  });
+
+  it("insert/remove_comment: can insert/remove a comment to db", function(){
+  	var thisid;
+    runs(function(){
+      var callback = function(err,doc){
+        expect(doc.shopid).toEqual(ExistID);
+        thisid = doc._id;
+      }
+      cs.insert_comment(doc, category, callback)
+    })
+    waits(100);
+    runs(function(){
+      cs.remove_comment_by_id(thisid,category);
+    });
+    waits(50);
+  });
+
+  it("get_comments: can get a rank of comments from db",function(){
+    runs(function(){
+      var callback = function(err,docs){
+      	var num = 0;
+      	for (var index in docs){
+          //logger.info("--->: "+docs[index]);
+      	  expect(docs[index].comment).toEqual("This shop is great!");
+      	  num++;
+      	}
+      	expect(num).toEqual(4);
+      };
+      cs.get_comments(ExistID,"Dining",1,4,callback);
+    });
+    waits(100);
+  });
+
+
 });
--- src/easydian/model/comment_schema.coffee
+++ src/easydian/model/comment_schema.coffee
@@ -6,20 +6,30 @@ class Comment_Schema
       comment: {type:String, default:'context'},
       createtime: {type:Date, default: Date.now}
     })
-  insert_comment: (comment,category) ->
-    @comm_model = Mongoose.model "comments", @comm_schema, category
-    comment_doc = new @comm_model comment
-    comment_doc.save (err,doc) ->
-      logger.info "failed to insert_comment: " + err  if err?
 
-  get_comments: (id,category,start,limit,callback) ->
-    conn.db.collection category, (err, collec) ->
-      collec.find({shopid:id})
-      .skip(start)
-      .limit(limit)
-      .sort('-createtime')
-      .select('news newsurl')
-      .exec(callback)
+  insert_comment: (comment,category,callback) ->
+    comm_model = Mongoose.model "comments", @comm_schema, category
+    comment_doc = new comm_model comment
+    if not callback?
+      comment_doc.save (err) ->
+        logger.info "failed to insert_comment: " + err  if err?
+    else
+      comment_doc.save callback
+    
+    
+  get_comments: (shopid,category,start,limit,callback) ->
+    cur_model = Mongoose.model "comments", @comm_schema, category
+    cur_model.find({shopid:shopid})
+    .sort('-createtime')
+    .skip(start)
+    .limit(limit)
+    .select("comment")
+    .exec(callback)
+
+  remove_comment_by_id: (id,category) ->
+    cur_model = Mongoose.model "comments", @comm_schema, category
+    cur_model.remove {_id:id}, (err) ->
+      logger.info "failed to remove_shop_by_id: "+err if err?
 
 
 module.exports = Comment_Schema
