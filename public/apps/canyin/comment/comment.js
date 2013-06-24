1c76008929202afd0cc648737a5c99b762ab190d
jundong <jundong.xu@spirent.com>
2013-06-24 03:32:30 -0700

    Using onClick event to replace the location.hash event

--- public/apps/canyin/comment/comment.js
+++ public/apps/canyin/comment/comment.js
@@ -31,36 +31,11 @@ steal(
                         element.append(can.view(comment_ejs_dir  + 'comment.ejs'));
                         $('#post').append(can.view(comment_ejs_dir  + 'post.ejs'));
                         $('#post_comments').append(can.view(comment_ejs_dir  + 'post_comment.ejs', data));
-                        $('#sidebar').append(can.view(comment_ejs_dir  + 'sidebar.ejs', {'data': data}));
-
-                        can.when(                        
-                            Models.CanyinComment.findAll({id: $.cookie("canyin_shop_id")}, function(data){
-                                for(var i = 0; i < data.length; i++){
-                                    if(data[i].reply_id.length == 0 || !data[i].inline) continue;
-                                    $('#' + data[i].reply_id).append(can.view(comment_ejs_dir  + 'post_inline_comment.ejs', data[i]));
-                                }                          
-                            })
-                        ).then(function(){
-                        });                    
+                        $('#sidebar').append(can.view(comment_ejs_dir  + 'sidebar.ejs', {'data': data}));                   
                     })
                 ).then(function(){                
                 });
-            }
-            else if(options.page === 'destroy') {
-                Models.Canyin.comment_destroy($.cookie("canyin_shop_id"), $.cookie("canyin_comment_id"), {}, function(data){
-                    if(data) {
-                        $('#' + $.cookie("canyin_comment_id")).remove();
-                        $.cookie("canyin_comment_id", null);
-                    }
-                })
-            } 
-            else if(options.page === 'update') {
-                can.when(
-                    Models.Canyin.comment_update({id: $.cookie("canyin_shop_id"), comment_id: $.cookie("canyin_comment_id")}, function(data){                        
-                    })
-                ).then(function(){
-                });
-            }   
+            }  
             else if(options.page === 'create') {
                 can.when(
                     Models.Canyin.comment_create({id: $.cookie("canyin_shop_id"), comment_id: $.cookie("canyin_comment_id")}, function(data){
@@ -74,7 +49,38 @@ steal(
         'li click': function(element) {
             if(element.attr('id') !== undefined)
                 $.cookie("canyin_comment_id", element.attr('id'));
-        },   
+            alert($.cookie("canyin_comment_id"));
+        },
+        'a[id*="update"] click': function(element) {            
+            if($.cookie("canyin_comment_id")) {
+                can.when(
+                    Models.Canyin.comment_update({id: $.cookie("canyin_shop_id"), comment_id: $.cookie("canyin_comment_id")}, function(data){                        
+                        if(element.attr('id') !== undefined){
+                            var comment_id = (element.attr('id')).split("_");
+                            $.cookie("canyin_comment_id", comment_id[1]);
+                        }
+                    })
+                ).then(function(){
+                    alert("update");
+                    $('#' + $.cookie("canyin_comment_id")).remove();
+                });                
+            }
+        }, 
+        'a[id*="delete"] click': function(element) {
+            if($.cookie("canyin_comment_id")) {
+                can.when(
+                    Models.Canyin.comment_destroy({id: $.cookie("canyin_shop_id"), comment_id: $.cookie("canyin_comment_id")}, function(data){                        
+                        if(element.attr('id') !== undefined){
+                            var comment_id = (element.attr('id')).split("_");                            
+                            $.cookie("canyin_comment_id", comment_id[1]);
+                        }                        
+                    })
+                ).then(function(){
+                    alert("delete");
+                    $('#' + $.cookie("canyin_comment_id")).remove();
+                });                
+            }
+        },                    
         get_current_user: function(current_user) {
             return defaults.current_user;
         }
